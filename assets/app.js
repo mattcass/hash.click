d61fe55c18022c4b75188c0603b268a69e4970d7
Matt Cass <matthewcasserly@gmail.com>
2017-05-25 16:30:01 -0400

    style updates, include links to npm and github

--- assets/app.js
+++ assets/app.js
@@ -1,15 +1,12 @@
-;(function ( w, doc ) {
-
+;(function(w, doc) {
   // Enable strict mode
-  'use strict';
-
-  var button = document.getElementById('button');
-  var box    = document.getElementsByClassName('box')[0];
+  'use strict'
+  var button = document.getElementById('button')
+  var box = document.getElementsByClassName('box')[0]
 
   function changeBG() {
-    box.style.backgroundColor = 'tomato';
+    box.style.backgroundColor = 'tomato'
   }
 
-  button.addEventListener('click', changeBG);
-
-})( this, this.document );
+  button.addEventListener('click', changeBG)
+})(this, this.document)
--- hash.click.js
+++ hash.click.js
@@ -5,47 +5,40 @@
 * Repo: https://github.com/mattcass/hash.click
 */
 
-;(function ( w, doc ) {
-
+;(function(w, doc) {
   // Enable strict mode
-  'use strict';
-
+  'use strict'
   // if there is a hash in the url, find a corresponding
   // href, data-href or aria-controls
   // and programmatically click it.
-  ( function hashClick() {
-
+  ;(function hashClick() {
     // wait for everything to be loaded
     w.onload = function() {
-
       // is there a #hash in the url?
-      if ( w.location.hash ) {
+      if (w.location.hash) {
+        var hashValue = w.location.hash
+        var el
 
-        var hashValue = w.location.hash;
-        var el;
-
-            // does the #hash match an attribute on the page?
-        var link = doc.querySelector("a[href='" + hashValue + "']");
-        var data = doc.querySelector("[data-href='" + hashValue + "']");
-        var aria = doc.querySelector("[aria-controls='" + hashValue.split('#')[1] + "']");
+        // does the #hash match an attribute on the page?
+        var link = doc.querySelector("a[href='" + hashValue + "']")
+        var data = doc.querySelector("[data-href='" + hashValue + "']")
+        var aria = doc.querySelector(
+          "[aria-controls='" + hashValue.split('#')[1] + "']"
+        )
 
         // set el to the appropriate element
-        if ( link != null ) {
-          el = link;
+        if (link != null) {
+          el = link
         }
-        if ( data != null ) {
-          el = data;
+        if (data != null) {
+          el = data
         }
-        if ( aria != null ) {
-          el = aria;
+        if (aria != null) {
+          el = aria
         }
 
-        el.click();
-
+        el.click()
       } // End if
-
     } // End window.onload
-
-  })();
-
-})( this, this.document );
+  })()
+})(this, this.document)
